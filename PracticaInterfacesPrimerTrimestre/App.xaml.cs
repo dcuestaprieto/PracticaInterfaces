9892de89f303759aecef1b768d4ff5aec8327233
Yutopya <Yuri.maximino.navarro@gmail.com>
2023-11-30 19:28:07 +0100

    windows size and ui color changes

--- PracticaInterfacesPrimerTrimestre/App.xaml.cs
+++ PracticaInterfacesPrimerTrimestre/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using PracticaInterfacesPrimerTrimestre.Repositorios;
-
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
 namespace PracticaInterfacesPrimerTrimestre;
 
 public partial class App : Application
@@ -14,9 +16,23 @@ public partial class App : Application
 		UsuarioRepositorio = usuarioRepositorio;
 		InitializeComponent();
 
-
+        UsuarioRepositorio = usuarioRepositorio;
+        InitializeComponent();
+        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+        {
+            var mauiWindow = handler.VirtualView;
+            var nativeWindow = handler.PlatformView;
+            nativeWindow.Activate();
+            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+            AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+            var presenter = appWindow.Presenter as OverlappedPresenter;
+            presenter.IsResizable = false;
+        });
 
         MainPage = new AppShell();
 	}
+
 
 }
